804e271651a44607dc777b462ada2280cc006900
Ethan Li <74832262+ethan-li-23333@users.noreply.github.com>
2021-07-28 09:54:08 -0600

    Update cloud9_frontend.py

--- cloud9_frontend.py
+++ cloud9_frontend.py
@@ -2,7 +2,6 @@ import os
 import requests
 from time import sleep
 
-# while(1):
 backend_url = "http://localhost:8000"
 
 file_path = "testphoto/"
@@ -17,6 +16,3 @@ body = {
 res = requests.post(backend_url + f"/process/", json=body)
 res = res.json()
 print(res)
-#     for i in range(300):
-#         print(i)
-#         sleep(1)
