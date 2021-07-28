6a2454ffb478ccf4e253828dcde27de759f03b6e
Ethan Li <74832262+ethan-li-23333@users.noreply.github.com>
2021-07-28 11:27:09 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+source env/bin/activate
+pip install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements.txt
+deactivate
